821596a1d22723f3e55b966d6b39d0be00222212
GouravSaini125 <rahul777singh000@gmail.com>
2019-12-31 20:48:49 +0530

    asd

--- edu/models.py
+++ edu/models.py
@@ -4,5 +4,4 @@ from quizApp.models import Module
 
 class Note(models.Model):
     module = models.ForeignKey(Module, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
     doc = models.FileField(upload_to='notes/')
