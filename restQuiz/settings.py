f1b4ef981dd265a184fc19512ca07cd95cf94e9e
GouravSaini125 <rahul777singh000@gmail.com>
2019-12-31 12:32:00 +0530

    edu

--- restQuiz/settings.py
+++ restQuiz/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-# import dj_database_url
-# import django_heroku
+import dj_database_url
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -142,8 +142,8 @@ MEDIA_URL = '/media/'
 #         'rest_framework.permissions.IsAuthenticated', )
 # }
 
-# django_heroku.settings(locals())
-# prod_db = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(prod_db)
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+django_heroku.settings(locals())
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 AUTH_USER_MODEL = 'account.User'
